38469eac4a0cefdbff0ff12182f1442395928eef
Eryck Kaique <eryckknl@hotmail.com>
2021-02-15 22:17:04 -0300

    edicao
    edicao

--- digionebanck/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digionebanck/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,11 +4,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Eryck"
     var cpf: String = "123.456.789-25"
+
+    constructor()
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val eryck = Pessoa()
-    println(eryck.nome)
-    println(eryck.cpf)
 
+    println(eryck.pessoaInfo())
 }

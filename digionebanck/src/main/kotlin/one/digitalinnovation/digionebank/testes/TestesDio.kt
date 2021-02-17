9dac0e17a987dc7cf2f44cc47e47145254db5400
Eryck Kaique <eryckknl@hotmail.com>
2021-02-17 00:18:05 -0300

    testes
    teste

---
+++ digionebanck/src/main/kotlin/one/digitalinnovation/digionebank/testes/TestesDio.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digionebank.testes
+
+fun main(args: Array<String>) {
+    //val a = input.nextInt()
+    val x: Int
+
+    x.rangeTo(8)
+
+    for (i in 1..100 .rangeTo(readLine()!!.toInt()).step(1)) println(i) //complete o codigo com os valores corretos
+    /*for (i in 1..100) {
+        println(a)
+    }
+    */
+
+
+}

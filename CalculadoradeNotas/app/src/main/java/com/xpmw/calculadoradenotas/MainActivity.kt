9b0785cfe560619c23f9ecb178ac6196d20bba60
Eryck Kaique <eryckknl@hotmail.com>
2021-02-18 22:45:12 -0300

    Calculando
    Projeto calculando

--- CalculadoradeNotas/app/src/main/java/com/xpmw/calculadoradenotas/MainActivity.kt
+++ CalculadoradeNotas/app/src/main/java/com/xpmw/calculadoradenotas/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.xpmw.calculadoradenotas
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity)
+
+        val btCalcular :Button = btn_calcular
+        val txtResultado : TextView = txt_resultado
+
+
+        btCalcular.setOnClickListener{
+            val nota01: Int = Integer.parseInt(editTextNota01.text.toString())
+            val nota02: Int = Integer.parseInt(editTextNota02.text.toString())
+            val faltas: Int = Integer.parseInt(editTextFaltas.text.toString())
+            val media = (nota01 + nota02)/2
+
+            if(media >= 7 && faltas <= 10){
+                txtResultado.setText("Aluno aprovado!" + "\n" + "Média Final: " + media + "\n" + "Faltas: " + faltas)
+                txtResultado.setTextColor(Color.GREEN)
+            }else{
+                txtResultado.setText("Aluno Reprovado!" + "\n" + "Média Final: " + media + "\n" + "Faltas: " + faltas)
+                txtResultado.setTextColor(Color.RED)
+            }
+        }
+
     }
 }

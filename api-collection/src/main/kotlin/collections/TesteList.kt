6dfb8a571e1926f803c98b40ec70c6aed3457235
Eryck Kaique <eryckknl@hotmail.com>
2021-02-19 22:35:16 -0300

    GroupBy
    GroupBy

--- api-collection/src/main/kotlin/collections/TesteList.kt
+++ api-collection/src/main/kotlin/collections/TesteList.kt
@@ -1,11 +1,11 @@
 package collections
 
 fun main(){
-    val joao = Funcionario("Joao", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria", 4000.0)
-    val carlos = Funcionario("Carlos", 3000.0)
-    val augusto = Funcionario("Augusto", 1800.0)
+    val joao = Funcionario("Joao", 1000.0, "CLT")
+    val pedro = Funcionario("Pedro", 2000.0, "CLT")
+    val maria = Funcionario("Maria", 5000.0, "PJ")
+    val carlos = Funcionario("Carlos", 4000.0, "PJ")
+    val augusto = Funcionario("Augusto", 1800.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria, carlos, augusto)
 
@@ -18,12 +18,17 @@ fun main(){
     println("**********")
 
     funcionarios.sortedBy { it.salario }.forEach{println(it)}
+
+    println("**********")
+
+    funcionarios.groupBy { it.tipoContratacao }.forEach{println(it)}
 }
 
-data class Funcionario(val nome: String, val salario: Double){
+data class Funcionario(val nome: String, val salario: Double, val tipoContratacao: String){
     override fun toString(): String = """
         Nome: $nome
         Salário: $salario
+        Categoria: $tipoContratacao
     """.trimIndent()
 }
 

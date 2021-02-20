db153ac9e5e1622b13cf3c271fcb26c84866e2ac
Eryck Kaique <eryckknl@hotmail.com>
2021-02-19 22:29:25 -0300

    new order
    new order

--- api-collection/src/main/kotlin/collections/TesteList.kt
+++ api-collection/src/main/kotlin/collections/TesteList.kt
@@ -4,14 +4,20 @@ fun main(){
     val joao = Funcionario("Joao", 1000.0)
     val pedro = Funcionario("Pedro", 2000.0)
     val maria = Funcionario("Maria", 4000.0)
+    val carlos = Funcionario("Carlos", 3000.0)
+    val augusto = Funcionario("Augusto", 1800.0)
 
-    val funcionarios = listOf(joao, pedro, maria)
+    val funcionarios = listOf(joao, pedro, maria, carlos, augusto)
 
     funcionarios.forEach{println(it)}
 
     println("**********")
 
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("**********")
+
+    funcionarios.sortedBy { it.salario }.forEach{println(it)}
 }
 
 data class Funcionario(val nome: String, val salario: Double){

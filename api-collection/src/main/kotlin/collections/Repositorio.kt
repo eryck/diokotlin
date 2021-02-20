0bbd407f25a413691312de9835ec572c1dd4431c
Eryck Kaique <eryckknl@hotmail.com>
2021-02-20 13:50:28 -0300

    Mutable Map
    Mutable Map

---
+++ api-collection/src/main/kotlin/collections/Repositorio.kt
@@ -0,0 +1,11 @@
+package collections
+
+class Repositorio<T> {
+    private val map = mutableMapOf<String, T>()
+
+    fun creat(id: String, value: T){
+        map[id] = value
+    }
+
+    fun findById(id: String)= map[id]
+}
---
+++ api-collection/src/main/kotlin/collections/TesteMutableMap.kt
@@ -0,0 +1,19 @@
+package collections
+
+fun main(){
+    val joao = Funcionario("Joao", 1000.0, "CLT")
+    val pedro = Funcionario("Pedro", 2000.0, "CLT")
+    val maria = Funcionario("Maria", 5000.0, "PJ")
+    val carlos = Funcionario("Carlos", 4000.0, "PJ")
+    val augusto = Funcionario("Augusto", 1800.0, "CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.creat(joao.nome, joao)
+    repositorio.creat(pedro.nome, pedro)
+    repositorio.creat(maria.nome, maria)
+    repositorio.creat(carlos.nome, carlos)
+    repositorio.creat(augusto.nome, augusto)
+
+    println(repositorio.findById(joao.nome))
+}

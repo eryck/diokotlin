e86165a1e4a220de0cea1113da37a438f2953d81
Eryck Kaique <eryckknl@hotmail.com>
2021-02-21 22:09:02 -0300

    Finish IMC app
    Finish IMC app

--- imcapp/app/src/main/java/com/xpmw/imc_app/MainActivity.kt
+++ imcapp/app/src/main/java/com/xpmw/imc_app/MainActivity.kt
@@ -2,10 +2,38 @@ package com.xpmw.imc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        alturaEDT?.doAfterTextChanged { text ->
+            //Envia o texto digitado para um Toast
+            //Toast.makeText(this, text.toString(), Toast.LENGTH_LONG).show()
+        }
+        pesoEDT?.doOnTextChanged{ text, _, _, _ ->
+            //envia o Texto digitado no Titulo
+            //titleTXT?.text = text
+        }
+        calcularBTN?.setOnClickListener{
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if(peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC é: \n $imc".format(imc)
+        }
     }
 }

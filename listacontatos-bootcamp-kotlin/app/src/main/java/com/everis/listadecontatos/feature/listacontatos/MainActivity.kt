a92a73d6871f83eba3be0f44183954fe0c3c4e7c
Eryck Kaique <eryckknl@hotmail.com>
2021-03-01 21:27:34 -0300

    Converte ContatoAplication
    Converte ContatoAplication in singleton

--- listacontatos-bootcamp-kotlin/app/src/main/java/com/everis/listadecontatos/feature/listacontatos/MainActivity.kt
+++ listacontatos-bootcamp-kotlin/app/src/main/java/com/everis/listadecontatos/feature/listacontatos/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.everis.listadecontatos.R
+import com.everis.listadecontatos.application.ContatoApplication
 import com.everis.listadecontatos.bases.BaseActivity
 import com.everis.listadecontatos.feature.contato.ContatoActivity
 import com.everis.listadecontatos.feature.listacontatos.adapter.ContatoAdapter
@@ -62,6 +63,7 @@ class MainActivity : BaseActivity() {
     private fun onClickBuscar(){
         val busca = etBuscar.text.toString()
         var listaFiltrada: List<ContatosVO> = ContatoSingleton.lista
+        listaFiltrada = ContatoApplication.instance.helperDB?.buscarContatos(busca) ?: mutableListOf()
         adapter = ContatoAdapter(this,listaFiltrada) {onClickItemRecyclerView(it)}
         recyclerView.adapter = adapter
         Toast.makeText(this,"Buscando por $busca",Toast.LENGTH_SHORT).show()

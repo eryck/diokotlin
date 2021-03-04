a53870659785ef5b4825149535a6017d2565e769
Eryck Kaique <eryckknl@hotmail.com>
2021-03-03 23:15:18 -0300

    Class 03
    Classe 03

--- AgendaTK/app/src/main/java/com/xpmw/agendatk/ContactAdapter.kt
+++ AgendaTK/app/src/main/java/com/xpmw/agendatk/ContactAdapter.kt
@@ -1,5 +1,6 @@
 package com.xpmw.agendatk
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,18 +10,24 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContacAdapterViewHold
     private val list : MutableList<Contact> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContacAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContacAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContacAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
     class ContacAdapterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
+
+
+        fun bind(contact: Contact){
+
+        }
     }
 }

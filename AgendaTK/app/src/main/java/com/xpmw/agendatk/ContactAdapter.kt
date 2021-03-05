604ec8859b1aebadf3bbbdec71040a7c8c3c42d2
Eryck Kaique <eryckknl@hotmail.com>
2021-03-04 22:22:29 -0300

    Continue
    Criado o ListView e ajustado o layout para rodar o projeto

--- AgendaTK/app/src/main/java/com/xpmw/agendatk/ContactAdapter.kt
+++ AgendaTK/app/src/main/java/com/xpmw/agendatk/ContactAdapter.kt
@@ -1,8 +1,11 @@
 package com.xpmw.agendatk
 
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContacAdapterViewHolder>(){
@@ -22,12 +25,20 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContacAdapterViewHold
         return list.size
     }
 
+    fun updateList(List: List<Contact>){
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
     class ContacAdapterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-
-
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhotograph: ImageView = itemView.findViewById(R.id.iv_photografic)
 
         fun bind(contact: Contact){
-
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
         }
     }
 }
--- AgendaTK/app/src/main/java/com/xpmw/agendatk/MainActivity.kt
+++ AgendaTK/app/src/main/java/com/xpmw/agendatk/MainActivity.kt
@@ -2,10 +2,27 @@ package com.xpmw.agendatk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+
+    }
+
+    private fun bindView(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
     }
 }

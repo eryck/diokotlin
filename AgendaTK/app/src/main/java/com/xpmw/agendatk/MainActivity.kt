f9d1c8b6cd56751d83538edeed2a8c23d2566ca9
Eryck Kaique <eryckknl@hotmail.com>
2021-03-05 22:50:47 -0300

    Erro Toolbar
    Erro Toolbar

--- AgendaTK/app/src/main/java/com/xpmw/agendatk/MainActivity.kt
+++ AgendaTK/app/src/main/java/com/xpmw/agendatk/MainActivity.kt
@@ -2,6 +2,15 @@ package com.xpmw.agendatk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,11 +24,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         bindViews()
         updateList()
     }
+
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toogle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+    }
+
 
     private fun bindViews(){
         rvList.adapter = adapter
@@ -41,5 +62,29 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item_menu_1 -> {
+                showToast("Menu 1")
+                return true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Menu 2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }

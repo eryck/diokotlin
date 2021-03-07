6f82cb559a584f00867155a5ef8c18fd974783bd
Eryck Kaique <eryckknl@hotmail.com>
2021-03-07 01:06:04 -0300

    gson
    gson

--- AgendaTK/app/src/main/java/com/xpmw/agendatk/MainActivity.kt
+++ AgendaTK/app/src/main/java/com/xpmw/agendatk/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.xpmw.agendatk
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,11 +12,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.edit
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.xpmw.agendatk.ContactDatailActivity.Companion.EXTRA_CONTACT
-import java.time.Instant
 
 class MainActivity : AppCompatActivity(), ClickItemContactListener {
 
@@ -29,41 +33,67 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         setContentView(R.layout.drawer_menu)
 
         initDrawer()
+        feychListContact()
         bindViews()
-        updateList()
+        //updateList()
     }
 
-    private fun initDrawer(){
+    private fun feychListContact() {
+        val list = arrayListOf(
+            Contact(
+                "Eryck Kaique",
+                "(63) 89999-9999",
+                "img.png"
+            ),
+            Contact(
+                "João Jose",
+                "(63) 85555-2545",
+                "img.png"
+            )
+        )
+        getInstaceSharedPreferences().edit {
+            val json = Gson().toJson(list)
+            putString("contacts", json)
+            commit()
+        }
+    }
+
+    private fun getInstaceSharedPreferences(): SharedPreferences {
+        return getSharedPreferences("com.xpmw.agendatk.PREFERENCES", Context.MODE_PRIVATE)
+    }
+
+    private fun initDrawer() {
         val drawerLauot = findViewById<View>(R.id.drawer_lauout) as DrawerLayout
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(this, drawerLauot, toolbar, R.string.open_drawer, R.string.close_drawer)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLauot,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
         drawerLauot.addDrawerListener(toggle)
         toggle.syncState()
     }
 
 
-    private fun bindViews(){
+    private fun bindViews() {
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
     }
 
-    private fun updateList(){
-        adapter.updateList(
-            arrayListOf(
-                Contact(
-                    "Eryck Kaique",
-                    "(63) 89999-9999",
-                    "img.png"
-                ),
-                Contact(
-                    "João Jose",
-                    "(63) 85555-2545",
-                    "img.png"
-                )
-            )
-        )
+    private fun getListContacts(): List<Contact> {
+        val list = getInstaceSharedPreferences().getString("contacts", "[]")
+        val turnsType = object : TypeToken<List<Contact>>() {}.type
+        return Gson().fromJson(list, turnsType)
+    }
+
+    private fun updateList() {
+        val list = getListContacts()
+        adapter.updateList(list)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -72,13 +102,13 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         return true
     }
 
-    private fun showToast(message: String){
+    private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        return when(item.itemId){
+        return when (item.itemId) {
             R.id.item_menu_1 -> {
                 showToast("Menu 1")
                 return true
@@ -91,7 +121,7 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         }
     }
 
-    override fun clickItemContact(contact: Contact){
+    override fun clickItemContact(contact: Contact) {
         val intent = Intent(this, ContactDatailActivity::class.java)
         intent.putExtra(EXTRA_CONTACT, contact)
         startActivity(intent)

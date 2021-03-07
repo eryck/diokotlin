359915e921161087d7741e768ef96d206fd9ed4a
Eryck Kaique <eryckknl@hotmail.com>
2021-03-07 09:47:52 -0300

    VModel Count
    VModel Count

--- VModel/app/src/main/java/com/xpmw/vmodel/MainActivity.kt
+++ VModel/app/src/main/java/com/xpmw/vmodel/MainActivity.kt
@@ -2,10 +2,44 @@ package com.xpmw.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtContador: EditText
+    lateinit var btnDados: Button
+    lateinit var btnMostrar: Button
+
+    var contador: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initDados()
+        initContador()
+        initClick()
+    }
+
+    private fun initClick() {
+        btnDados.setOnClickListener{
+            contador ++
+            initContador()
+        }
+        btnMostrar.setOnClickListener{
+            Toast.makeText(this, "Valor: ${contador.toString()}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initContador() {
+        txtContador.setText(contador.toString())
+    }
+
+    private fun initDados() {
+        txtContador = findViewById(R.id.editContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
     }
 }

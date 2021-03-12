f3ad9bfe2c8897eaa73c11a2034cf4c2939d0942
Eryck Kaique <eryckknl@hotmail.com>
2021-03-11 21:43:35 -0300

    teste
    teste

---
+++ api-collection/src/main/kotlin/collections/teste.kt
@@ -0,0 +1,15 @@
+package collections
+
+val r = 199F
+var i = 0F
+
+if ()  ) {
+    i =
+} else if () {
+    i =
+} else if (  ) {
+    i =
+} else if (r > 4500) {
+    i = (  )
+}
+if (i == 0F) println("Isento") else println("$i")

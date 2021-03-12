728e4d0bf42f9be32e05146f5723540835f13b1e
Eryck Kaique <eryckknl@hotmail.com>
2021-03-12 20:34:52 -0300

    Tax Challenge
    Tax Challenge

--- api-collection/src/main/kotlin/collections/teste.kt
+++ api-collection/src/main/kotlin/collections/teste.kt
@@ -1,17 +1,20 @@
 package collections
 fun main() {
 
-    val r = 3002.00F
+    val r = 4520.00F
     var i = 0F
 
     if (r <= 2000.00F) {
         i= 0F
-    }else if (r >= 2000.01F || r <= 3000.00F){
+    }else if (r <= 3000.00F){
        var x = r - 2000.00F
-        i = x * 0.8F
-    }else if (r >= 3000.01F || r <= 4500.00F){
-        var x = r - 2000.00F
-        i = x * 0.18F
+        i = x * 0.08F
+    }else if (r <= 4500.00F){
+        var x = r - 3000.00F
+        i = (x * 0.18F) + 80.00F
+    }else if (r >= 4500.00F){
+        var x = r - 4500.00F
+        i = (x * 0.28F) + 350.00F
     }
 
     if (i == 0F) println("Insento") else print("R$ $i")

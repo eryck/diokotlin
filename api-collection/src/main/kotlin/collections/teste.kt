507920d343d0159efe61765d740c42c09a06b6bc
Eryck Kaique <eryckknl@hotmail.com>
2021-03-11 22:47:38 -0300

    teste
    teste

--- api-collection/src/main/kotlin/collections/teste.kt
+++ api-collection/src/main/kotlin/collections/teste.kt
@@ -1,11 +1,17 @@
 package collections
 fun main() {
 
-    val r = 1701.12F
+    val r = 3002.00F
     var i = 0F
 
-    if (r <= 2000.00f) {
+    if (r <= 2000.00F) {
         i= 0F
+    }else if (r >= 2000.01F || r <= 3000.00F){
+       var x = r - 2000.00F
+        i = x * 0.8F
+    }else if (r >= 3000.01F || r <= 4500.00F){
+        var x = r - 2000.00F
+        i = x * 0.18F
     }
 
     if (i == 0F) println("Insento") else print("R$ $i")

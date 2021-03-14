0c1e2e5d398df88efa4bc7249a94e9fc4daca770
Eryck Kaique <eryckknl@hotmail.com>
2021-03-13 22:14:39 -0300

    question calc area
    question calc area

--- api-collection/src/main/kotlin/collections/AreaCircle.kt
+++ api-collection/src/main/kotlin/collections/AreaCircle.kt
@@ -1,5 +1,14 @@
 package collections
 
-fun main(){
-    
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+fun main() {
+
+    val raio = 2
+    val pi = 3.14
+    val area = 3
+    val resultado = BigDecimal(area).setScale(0, RoundingMode.HALF_EVEN)
+    println("A= $resultado")
+
 }

22248d5dc8d78248c93d4c40f584e20251e0375a
Eryck Kaique <eryckknl@hotmail.com>
2021-03-15 22:27:23 -0300

    ok
    ok

--- api-collection/src/main/kotlin/collections/PrimeNumbers.kt
+++ api-collection/src/main/kotlin/collections/PrimeNumbers.kt
@@ -1,21 +1,35 @@
 package collections
 
+import kotlin.math.sqrt
+
 fun main(){
 
-    var n = 1
-    var test = false
+    var n = 4
 
+    var x = (1..8).shuffled().first()
+    var result = isPrime(x)
+
+
+/*
     for(i in 1..n){
-        var x = 5
-        for (j in 2..x / 2){
-            if(x % j == 0){
+        //var x = (1..8).shuffled().first()
+        var x: Double = 0.444
+        println(x)
+        var raiz = sqrt(x.toDouble()).toInt()
+        var test = false
+        for (j in 2..raiz) {
+            if(x.toInt() % j == 0){
                 test = true
+                println("Not Prime")
+                break
             }
-            false
-
+            //println("Prime")
         }
-        if(!test){
-            println("Prime")
-        }else println("Not Prime")
+        if(!test)println("Prime")
+        //else println("Not Prime")
     }
+ */
+
+
+
 }

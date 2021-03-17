05ef2674f24590424f3cc8265a5dc2a926930f43
Eryck Kaique <eryckknl@hotmail.com>
2021-03-16 21:47:13 -0300

    turtle
    turtle

--- api-collection/src/main/kotlin/collections/PrimeNumbers.kt
+++ api-collection/src/main/kotlin/collections/PrimeNumbers.kt
@@ -6,11 +6,6 @@ fun main(){
 
     var n = 4
 
-    var x = (1..8).shuffled().first()
-    var result = isPrime(x)
-
-
-/*
     for(i in 1..n){
         //var x = (1..8).shuffled().first()
         var x: Double = 0.444
@@ -28,8 +23,32 @@ fun main(){
         if(!test)println("Prime")
         //else println("Not Prime")
     }
- */
+
 
 
 
 }
+
+/*
+fun main(args: Array<String>) {
+    //continue a solução
+    val n = readLine()!!.toInt()
+
+    for (i in 1..n) {
+        val x = readLine()!!.toDouble()
+        val raiz = sqrt(x).toInt()
+        var test = false
+        for (j in 2..raiz){
+            if((x.toInt() % j) == 0){
+                test = true
+                println("Not Prime")
+                break
+            }
+
+        }
+        if(!test)println("Prime")
+        //else println("Not Prime")
+    }
+}
+
+ */
---
+++ api-collection/src/main/kotlin/collections/TurtleRunning.kt
@@ -0,0 +1,24 @@
+package collections
+
+fun main(){
+
+    while (true) {
+
+        try {
+
+            val l = readLine()!!.toInt()
+            val v = readLine()!!.split("        ").run {     {      () } }
+            v.sort()
+            when (v.last()) {
+
+            }
+
+        } catch (f :  ) {
+            break
+        } catch (n : ) {
+            break
+        } catch (e :) {
+            break
+        }
+    }
+}

8134a109f6c31171212980007e33e199c7458d2f
Eryck Kaique <eryckknl@hotmail.com>
2021-03-27 14:44:55 -0300

    Teste Jogo Matemático
    Teste Jogo Matemático

--- CodKotlin/src/main/kotlin/JogoMatematico.kt
+++ CodKotlin/src/main/kotlin/JogoMatematico.kt
@@ -1,4 +1,33 @@
+fun main(args: Array<String>) {
 
+    val N = readLine()!!.toInt()
+
+    for (i in 1..N) {
+
+        var leitor: List<String> = readLine()!!.split(" ")
+
+        for(i in leitor){
+
+            var N1 = i.substring(2,3).toInt()
+            var N2 = i.substring(0,1).toInt()
+
+
+            if (N1 == N2) {
+                println(N1 * N2)
+            } else {
+                if(i.contentEquals(i.toUpperCase()) && N2 != N1){
+                    println(N1 - N2)
+                }else{
+                    println(N1 + N2)
+                }
+            }
+
+        }
+
+    }
+}
+
+/* Código Original do teste
 fun main(args: Array<String>) {
 
     val N = readLine()!!.toInt()
@@ -9,4 +38,5 @@ fun main(args: Array<String>) {
 
     }
 }
+ */
 

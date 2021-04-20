3646f267fea56996e9e6f703d972151bd8519717
Eryck Kaique <eryckknl@hotmail.com>
2021-04-20 00:37:21 -0300

    Firestore Project
    Firestore Project

---
+++ FirestoreProject/app/src/main/java/br/com/xpmw/firestoreproject/RecyclerviewActivity.kt
@@ -0,0 +1,11 @@
+package br.com.xpmw.firestoreproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recyclerview)
+    }
+}

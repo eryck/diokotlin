10973f830b96ed1d536ea97500bbf8e16eb723bf
Eryck Kaique <eryckknl@hotmail.com>
2021-05-31 22:40:07 -0300

    Setting Up the Login Activity
    Setting Up the Login Activity

--- MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/LoginActivity.kt
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/LoginActivity.kt
@@ -1,12 +1,29 @@
 package br.com.xpmw.myshoppal.activities
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import br.com.xpmw.myshoppal.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setUpFullScreen()
+    }
+
+    private fun setUpFullScreen() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
--- MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/SplashActivity.kt
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/SplashActivity.kt
@@ -27,7 +27,7 @@ class SplashActivity : AppCompatActivity() {
         @Suppress("DEPRECATION")
         Handler().postDelayed(
             {
-                startActivity(Intent(this, MainActivity::class.java))
+                startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             },
         1500

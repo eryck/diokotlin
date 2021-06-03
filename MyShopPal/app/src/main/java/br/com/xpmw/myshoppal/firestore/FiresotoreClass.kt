9b0a1a815756cac3c684708e6e7d90b4e94541f7
Eryck Kaique <eryckknl@hotmail.com>
2021-06-03 13:39:45 -0300

    Setting Firestore in project
    Setting Firestore in project

---
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/firestore/FiresotoreClass.kt
@@ -0,0 +1,7 @@
+package br.com.xpmw.myshoppal.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FiresotoreClass {
+    private val mFirestore = FirebaseFirestore.getInstance()
+}

d3f0c5ef65d57031810be19c7b90327f6879f485
Eryck Kaique <eryckknl@hotmail.com>
2021-06-03 00:15:30 -0300

    Forgot Password Implementation
    Forgot Password Implementation

--- MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/ForgotPasswordActivity.kt
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/ForgotPasswordActivity.kt
@@ -2,11 +2,13 @@ package br.com.xpmw.myshoppal.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.xpmw.myshoppal.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 import kotlinx.android.synthetic.main.activity_register.*
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class ForgotPasswordActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
@@ -25,5 +27,27 @@ class ForgotPasswordActivity : AppCompatActivity() {
         }
 
         toolbar_forgot_password_activity.setNavigationOnClickListener { onBackPressed() }
+        btn_submit.setOnClickListener {
+            val email: String = et_email_forgot_pw.text.toString().trim() { it <= ' ' }
+            if (email.isEmpty()) {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+            } else {
+                showProgressDialog(resources.getString(R.string.please_wait))
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        hideProgressDialog()
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this@ForgotPasswordActivity,
+                                resources.getString(R.string.email_sent_success),
+                                Toast.LENGTH_LONG
+                            )
+                            finish()
+                        } else {
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+            }
+        }
     }
 }

8084435c717579cbe3f3f464a54933da86584578
Eryck Kaique <eryckknl@hotmail.com>
2021-06-04 22:19:56 -0300

    Profile Activity
    Profile Activity

---
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package br.com.xpmw.myshoppal.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.xpmw.myshoppal.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}

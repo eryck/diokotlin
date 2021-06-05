56be097a7c5b49b54256306e62852e3c7a0b4eca
Eryck Kaique <eryckknl@hotmail.com>
2021-06-05 16:37:37 -0300

    Permission Read Storage
    Permission Read Storage

--- MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/UserProfileActivity.kt
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/activities/UserProfileActivity.kt
@@ -1,19 +1,26 @@
 package br.com.xpmw.myshoppal.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import br.com.xpmw.myshoppal.R
 import br.com.xpmw.myshoppal.model.User
+import br.com.xpmw.myshoppal.utils.Constants
 import br.com.xpmw.myshoppal.utils.Constants.EXTRA_USER_DETAILS
+import br.com.xpmw.myshoppal.utils.Constants.READ_STORAGE_PERMISSION_CODE
 import kotlinx.android.synthetic.main.activity_user_profile.*
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
 
         var userDatails: User = User()
-        if (intent.hasExtra(EXTRA_USER_DETAILS)){
+        if (intent.hasExtra(EXTRA_USER_DETAILS)) {
             userDatails = intent.getParcelableExtra(EXTRA_USER_DETAILS)!!
         }
 
@@ -23,5 +30,44 @@ class UserProfileActivity : AppCompatActivity() {
         et_last_name.setText(userDatails.lastName)
         et_email.isEnabled = false
         et_email.setText(userDatails.email)
+
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_user_photo -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this, Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        showErrorSnackBar("You already have the storage permission", false)
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE){
+            if (grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showErrorSnackBar("The storage permission is granted", false)
+            }else{
+                Toast.makeText(this, resources.getString(R.string.read_storage_permission_denied), Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }

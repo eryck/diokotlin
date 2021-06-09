082209d022ece7cdeb92290ae2246d6b51c14344
Eryck Kaique <eryckknl@hotmail.com>
2021-06-08 22:42:56 -0300

    Preparing the Add Product Activity and the Menu that Leads there
    Preparing the Add Product Activity and the Menu that
    Leads there

--- MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/ui/fragments/ProductsFragment.kt
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/ui/fragments/ProductsFragment.kt
@@ -1,14 +1,19 @@
 package br.com.xpmw.myshoppal.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import br.com.xpmw.myshoppal.R
+import br.com.xpmw.myshoppal.ui.activities.AddProductActivity
 
 class ProductsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +24,22 @@ class ProductsFragment : Fragment() {
         val textView: TextView = root.findViewById(R.id.text_products)
         textView.text = "This is Products Fragment"
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_product_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when(id){
+            R.id.action_add_product ->{
+                startActivity(Intent(activity, AddProductActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

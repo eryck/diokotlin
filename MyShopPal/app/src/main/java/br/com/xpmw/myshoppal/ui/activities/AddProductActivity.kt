1a08cadc98d5fe29ae6551546c5a4e5aaad37b64
Eryck Kaique <eryckknl@hotmail.com>
2021-06-12 11:33:30 -0300

    Let the User Select a Product Image
    Let the User Select a Product Image

--- MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/ui/activities/AddProductActivity.kt
+++ MyShopPal/app/src/main/java/br/com/xpmw/myshoppal/ui/activities/AddProductActivity.kt
@@ -1,27 +1,109 @@
 package br.com.xpmw.myshoppal.ui.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import br.com.xpmw.myshoppal.R
+import br.com.xpmw.myshoppal.utils.Constants
+import br.com.xpmw.myshoppal.utils.GliderLoader
 import kotlinx.android.synthetic.main.activity_add_product.*
+import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.io.IOException
 
-class AddProductActivity : AppCompatActivity() {
+class AddProductActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
 
         setupActionBar()
+        iv_add_update_product.setOnClickListener(this)
     }
 
-    private fun setupActionBar(){
+    private fun setupActionBar() {
         setSupportActionBar(toolbar_add_product_activity)
 
         val actionBar = supportActionBar
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
         }
 
         toolbar_add_product_activity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_update_product -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Constants.showImageChooser(this)
+                    } else {
+                        /*
+                            Request permissions to be granted to this application.
+                         */
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //showErrorSnackBar("The storage permission is granted", false)
+                Constants.showImageChooser(this)
+            } else {
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                   iv_add_update_product.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
+
+                    val selectedImageFileURI = data.data!!
+
+                    try {
+                        GliderLoader(this).loadUserPicture(selectedImageFileURI, iv_product_image)
+                    }catch (e: IOException){
+                        e.printStackTrace()
+                        Toast.makeText(this, getString(R.string.image_selection_failed), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Request Cancelled", "Image selection cancelled")
+        }
+    }
+
 }
